92fcb62711891e262df3d406380ca637368bb008
saxonredhat <saxon.redhat@gmail.com>
2016-07-26 23:32:17 +0800

    Update getPhone.sql

--- sql/getPhone.sql
+++ sql/getPhone.sql
@@ -1,8 +1,8 @@
 select 
 	a.phone 
 	from 
-	puhui8.tau_user_info a,
-        puhui8.TAU_USER_REGISTRY b
+	xxx.tau_user_info a,
+        xxx.TAU_USER_REGISTRY b
 	where
 	a.user_id=b.user_id
 	and b.REGISTRY_TIME BETWEEN TO_DATE('&1','yyyy-mm-dd') AND TO_DATE('&2','yyyy-mm-dd')
