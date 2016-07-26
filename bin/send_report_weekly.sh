3a6271f95eba05f575fb4df89df0a60b2b47af72
saxonredhat <saxon.redhat@gmail.com>
2016-07-26 23:30:11 +0800

    Update send_report_weekly.sh

--- bin/send_report_weekly.sh
+++ bin/send_report_weekly.sh
@@ -7,10 +7,10 @@ withdraw_prefix="提现"
 new_investment_prefix="新投资"
 capital_record_prefix="资金交易记录"
 REPORT_DIR="reports"
-mail_list="kefu@puhui8.com"
-mail_list2="zhouchunyan@puhui8.com"
+mail_list="kefu@xxx.com"
+mail_list2="zhouchunyan@xxx.com"
 start_time=`date "+%Y-%m-%d" --date="-7 day"`
 end_time=`date "+%Y-%m-%d"`
 show_start_time=$start_time
 show_end_time=`date -d "$end_time -1 day" "+%Y-%m-%d"`
-echo "普惠在线 http://www.puhui8.com"|mutt -s "平台${capital_record_prefix}($show_start_time-$show_end_time)" $mail_list2 -a $REPORT_DIR/${capital_record_prefix}_${show_start_time}_${show_end_time}.xls
+echo "xxx在线 http://www.xxx.com"|mutt -s "平台${capital_record_prefix}($show_start_time-$show_end_time)" $mail_list2 -a $REPORT_DIR/${capital_record_prefix}_${show_start_time}_${show_end_time}.xls
