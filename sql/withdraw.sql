1440c13bd3ac1f2783198f967107fbeb3d27fe51
saxonredhat <saxon.redhat@gmail.com>
2016-07-26 23:36:25 +0800

    Update withdraw.sql

--- sql/withdraw.sql
+++ sql/withdraw.sql
@@ -6,8 +6,8 @@ SELECT a.user_name 用户名,
           b.TRADE_AMOUNT 提现金额,
           b.DEAL_FEE 手续费,
           to_char(b.RESPONSE_TIME,'YYYY-MM-DD hh24:mi:ss') 提现时间
-        FROM puhui8.tau_user_info a,
-          puhui8.TAL_PAYMENTGATEWAY_RECODE b
+        FROM xxx.tau_user_info a,
+          xxx.TAL_PAYMENTGATEWAY_RECODE b
         WHERE a.user_id=b.user_id
         AND b.OPT_TYPE ='09'
         AND b.status   ='0000'
